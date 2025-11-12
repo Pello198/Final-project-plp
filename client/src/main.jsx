ebd6bf2ee0970ba30ea7d2b25af5b89132b9012c
pello <iankwemoi6@gmail.com>
2025-11-12 13:20:38 +0300

    added files

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,25 +1,19 @@
 import React from "react";
-import { createRoot } from "react-dom/client"; // Import createRoot
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
 import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
 
-// Initialize Sentry
 Sentry.init({
-  dsn:import.meta.env.VITE_SENTRY_DSN
-,
-  integrations: [new BrowserTracing()],
+  dsn:import.meta.env.VITE_SENTRY_DSN,
+  integrations: [Sentry.browserTracingIntegration()],
   tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1, // Optional: captures session replays
+  replaysOnErrorSampleRate: 1.0,
 });
 
-// Get root container
-const container = document.getElementById("root"); // make sure this matches your HTML
-const root = createRoot(container);
-
-// Render app
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
